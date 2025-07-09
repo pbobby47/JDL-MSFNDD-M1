9c41627b127de7f8f68424320e862e39928a26ee
Bobby Perecharla <perecharlababi47@gmail.com>
2025-07-09 11:32:32 +0530

    MongoDB Commands

---
+++ MongoDB/code.js
@@ -0,0 +1,32 @@
+db.ManavFrds.insertMany([
+  {
+    name: "sahil",
+    course: "MERN",
+    isPlaced: true,
+  },
+  {
+    name: "Ritanshu",
+    course: "Data Analytics",
+    isPlaced: false,
+  },
+  {
+    name: "Rahul",
+    course: "Python",
+    isPlaced: true,
+  },
+]);
+
+db.ManavFrds.insertMany([
+  {
+    _id: "frd-qsp-001",
+    name: "Shweta",
+    course: "Java",
+    isPlaced: false,
+  },
+  {
+    _id: "frd-qsp-002",
+    name: "Ajit",
+    course: "MERN",
+    isPlaced: true,
+  },
+]);
